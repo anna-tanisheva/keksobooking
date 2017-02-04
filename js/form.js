3b541fb58447f49a9e34a4c35caa921443797216
lisa2047 <lisa2047@yandex.ru>
2017-02-04 20:25:33 +0300

    Сделала валидацию формы

--- js/form.js
+++ js/form.js
@@ -16,6 +16,7 @@ var roomNumber = document.querySelector('#room_number');
 var roomNumberOptions = roomNumber.querySelectorAll('#room_number option');
 var capacity = document.querySelector('#capacity');
 var capacityOptions = capacity.querySelectorAll('#capacity option');
+var form = document.querySelector('.notice__form');
 
 function pinClick(item) {
   item.addEventListener('click', function () {
@@ -37,16 +38,58 @@ close.addEventListener('click', function () {
 });
 
 // Валидация формы
-title.setAttribute('required', true);
-title.setAttribute('minlength', 30);
-title.setAttribute('maxlength', 100);
 
-minPrice.setAttribute('required', true);
 minPrice.setAttribute('type', 'number');
-minPrice.setAttribute('min', 1000);
-minPrice.setAttribute('max', 1000000);
 
-address.setAttribute('required', true);
+var showError = function (container, errorMessage) {
+  container.classList.add('error');
+  var msgElem = document.createElement('span');
+  msgElem.className = 'error-message';
+  msgElem.innerHTML = errorMessage;
+  container.appendChild(msgElem);
+};
+
+var resetError = function (container) {
+  container.classList.remove('error');
+  if (container.lastChild.className === 'error-message') {
+    container.removeChild(container.lastChild);
+  }
+};
+
+var validate = function (form) {
+
+  resetError(form);
+  if (!title.value || title.value.length < 30 || title.value.length > 100) {
+    showError(form,
+      'Заголовок - обязательное для заполнения поле, длина сообщения - не менее 30 и не более 100 символов, сейчас '
+       + title.value.length);
+    title.classList.add('error');
+    return false;
+  }
+
+  resetError(form);
+  if (!address.value) {
+    showError(form, 'Адрес - обязательное для заполнения поле');
+    address.classList.add('error');
+    return false;
+  }
+
+  resetError(form);
+  if (!minPrice.value || minPrice.value < 1000 || minPrice.value > 1000000) {
+    showError(form,
+      'Цена за ночь - обязательное для заполнения поле, минимальная цена - 1.000, максимальная - 1.000.000');
+    minPrice.classList.add('error');
+    return false;
+  }
+
+  return true;
+};
+
+form.addEventListener('submit', function (evt) {
+  if (!validate(form)) {
+    evt.preventDefault();
+  }
+});
 
 // Зависимость полей время выезда и время заезда
 checkInTime.addEventListener('change', function () {
