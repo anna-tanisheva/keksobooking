7cc9b216e90f11cfc115c48bc7dd163e1e07483e
lisa2047 <lisa2047@yandex.ru>
2017-02-19 14:16:46 +0300

    Module6-task2

--- js/initialize-pins.js
+++ js/initialize-pins.js
@@ -2,32 +2,23 @@
 
 window.initializePins = (function () {
 
-  var pinActive = document.querySelector('.pin.pin--active');
-  var dialog = document.querySelector('.dialog');
-  var close = dialog.querySelector('.dialog__close');
   var map = document.querySelector('div.tokyo__pin-map');
 
   var ENTER_KEY_CODE = 13;
 
-  function openDialog(item) {
-    pinActive.classList.remove('pin--active');
-    item.classList.add('pin--active');
-    pinActive = item;
-    dialog.classList.remove('invisible');
-    item.setAttribute('aria-pressed', true);
-  }
+  window.curTarget;
 
-  function closeDialog() {
-    dialog.classList.add('invisible');
-    pinActive.classList.remove('pin--active');
-    pinActive.setAttribute('aria-pressed', false);
-  }
+  var obj = {};
 
-  return {
+  return obj = {
+    callBack: null,
+
+    curTarget: null,
+
     mapHandlerClick: function () {
       map.addEventListener('click', function (evt) {
         var target = evt.target.parentNode;
-        openDialog(target);
+        window.showCard(target);
       });
     },
 
@@ -35,21 +26,31 @@ window.initializePins = (function () {
       map.addEventListener('keydown', function (evt) {
         if (evt && evt.keyCode === ENTER_KEY_CODE) {
           var target = evt.target;
-          openDialog(target);
+          window.showCard(target);
+          obj.curTarget = target;
+          obj.callBack = function () {
+            obj.curTarget.focus();
+          }
         }
       });
     },
 
     closeClick: function () {
       close.addEventListener('click', function () {
-        closeDialog();
+        window.closeDialog();
+        if (typeof obj.callBack === 'function') {
+          obj.callBack();
+        }
       });
     },
 
     closeKeydown: function () {
       close.addEventListener('keydown', function (evt) {
         if (evt && evt.keyCode === ENTER_KEY_CODE) {
-          closeDialog();
+          window.closeDialog();
+          if (typeof obj.callBack === 'function') {
+            obj.callBack();
+          }
         }
       });
     }
